4c797939fbdac859cec1c2b8293fcb855bec016a
Vineet_1 <vineet@paralleldots.com>
2018-11-22 13:38:33 +0530

    Updates

---
+++ precache-manifest.c403009d353f1211658476ec7a94009c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d48617701e99ef55c92edb5e02022bb",
+    "url": "/contactManager/static/media/architecture-background-blank-158771.5d486177.jpg"
+  },
+  {
+    "revision": "c9c7142bbfeec04feca1",
+    "url": "/contactManager/static/js/runtime~main.c9c7142b.js"
+  },
+  {
+    "revision": "f4d2664c62408ba839f4",
+    "url": "/contactManager/static/js/main.f4d2664c.chunk.js"
+  },
+  {
+    "revision": "2e67e2583beb23c72cf7",
+    "url": "/contactManager/static/js/1.2e67e258.chunk.js"
+  },
+  {
+    "revision": "f4d2664c62408ba839f4",
+    "url": "/contactManager/static/css/main.90296e8e.chunk.css"
+  },
+  {
+    "revision": "2e67e2583beb23c72cf7",
+    "url": "/contactManager/static/css/1.b5097ca0.chunk.css"
+  },
+  {
+    "revision": "dc8347fe49f3bcd7637d286af6b309b5",
+    "url": "/contactManager/index.html"
+  }
+];
